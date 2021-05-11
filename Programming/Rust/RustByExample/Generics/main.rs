ac530f0d9f7bde39189effea76b0592e98a4c2dd
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-11 20:27:14 +0500

    Upload Generics chapter

---
+++ Programming/Rust/RustByExample/Generics/main.rs
@@ -0,0 +1,17 @@
+struct A;
+
+struct Single(A);
+
+struct SingleGen<T>(T);
+
+fn main() {
+    let _s = Single(A);
+
+    // Type parameter explicitly specified
+    let _char: SingleGen<char> = SingleGen('a');
+
+    // Implicitly:
+    let _t = SingleGen(A);
+    let _i32 = SingleGen(6);
+    let _char = SingleGen('a');
+}
