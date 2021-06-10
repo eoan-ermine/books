1678870a26e32fe12b2da49ed17db7fdb4aef258
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 15:55:56 +0500

    Add fields method example

--- Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/models/records/User.php
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/models/records/User.php
@@ -25,4 +25,27 @@ class User extends ActiveRecord
         $scenarios[self::SCENARIO_REGISTER] = ['username', 'email', 'password'];
         return $scenarios;
     }
+
+    public function fields()
+    {
+        return [
+            'id',
+            'email' => 'email_address',
+            // field name is "name", its value is defined by a PHP callback
+            'name' => function() {
+                return $this->first_name . ' ' . $this->last_name;
+            }
+        ];
+    }
+
+    // filter out some fields
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        // remove fields that contain sensitive information
+        unset($fields['auth_key'], $fields['password_hash'], $fields['password_reset_token']);
+
+        return $fields;
+    }
 }
