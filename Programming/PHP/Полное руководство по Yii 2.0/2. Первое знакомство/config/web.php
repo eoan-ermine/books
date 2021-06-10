834cac3e43a6e38fc264d0d975596de956bcdbfa
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 18:25:14 +0500

    Optimize publishing of assets by linking them, not copying

--- Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/config/web.php
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/config/web.php
@@ -12,6 +12,9 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'assetManager' => [
+            'linkAssets' => true,
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'eKchMkfzdO5jjxS8wbocaljcCyX3VUAp',
