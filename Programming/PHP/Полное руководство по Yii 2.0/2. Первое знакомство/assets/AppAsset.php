3ea2b07625180a588e47abd16cede7d4320ff9be
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 18:02:55 +0500

    Add example of specifying options to an asset

--- Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/assets/AppAsset.php
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/assets/AppAsset.php
@@ -19,11 +19,18 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
+
+    // public $cssOptions = ['condition' => 'lte IE9'];
+    // ^ Conditionally include a CSS file for browsers that are IE9 or below
     public $css = [
         'css/site.css',
     ];
+    
+    // public $jsOptions = ['position' => \yii\web\View::POS_HEAD];
+    // ^ Force including of JS files in the head section of a page
     public $js = [
     ];
+    
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
