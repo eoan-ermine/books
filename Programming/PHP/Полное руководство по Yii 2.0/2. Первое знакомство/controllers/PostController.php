a0f173e1ca07d7152dbcf47619f19cf63f9a46c6
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 15:05:40 +0500

    Upload Controller example

---
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\controlers;
+
+use Yii;
+use app\models\Post;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class PostController extends Controller
+{
+    public function actionView($id)
+    {
+        $model = Post::findOne($id);
+        if($model === null)
+        {
+            throw new NotFoundHttpException;
+        }
+
+        return $this->render('view', [
+            'model' => $model,
+        ]);
+    }
+    public function actionCreate()
+    {
+        $model = new Post;
+
+        if($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
