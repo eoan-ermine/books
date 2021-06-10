05424690ea1ddf2101af3878e2b81b6be84258a3
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 15:37:15 +0500

    Add rules example

--- Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/models/records/User.php
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/models/records/User.php
@@ -9,6 +9,15 @@ class User extends ActiveRecord
     const SCENARIO_LOGIN = 'login';
     const SCENARIO_REGISTER = 'register';
 
+    public function rules()
+    {
+        return [
+            [['username', 'email', 'password'], 'required', 'on' => self::SCENARIO_REGISTER],
+            [['username', 'password'], 'required', 'on' => self::SCENARIO_LOGIN],
+            ['email', 'email'],
+        ];
+    }
+
     public function scenarios()
     {
         $scenarios = parent::scenarios();
