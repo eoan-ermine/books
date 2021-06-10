9f26f4c8676db69b22886812f29d290676119375
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-10 18:26:37 +0500

    Enable cache busting, allows assets to cache normal

--- Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/config/web.php
+++ Programming/PHP/Полное руководство по Yii 2.0/2. Первое знакомство/config/web.php
@@ -14,6 +14,7 @@ $config = [
     'components' => [
         'assetManager' => [
             'linkAssets' => true,
+            'appendTimestamp' => true,
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
