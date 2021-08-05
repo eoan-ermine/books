222317e907e9f557ff3caaf99b529ad7076bacef
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-05 17:51:01 +0000

    Add chapter about designated initialization

---
+++ Programming/C++/C++20. Get the details/A Quick Overview of C++20/Core Language/Designated Initialization/aggregate_initialization.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct Point2D {
+    int x;
+    int y;
+};
+
+class Point3D {
+public:
+    int x;
+    int y;
+    int z;
+};
+
+int main() {
+    Point2D point2D {1, 2};
+    Point3D point3D {1, 2, 3};
+
+    std::cout << "point2D: " << point2D.x << " " << point2D.y << "\n";
+    std::cout << "point3D: " << point3D.x << " "
+              << point3D.y << " " << point3D.z << '\n';
+}
