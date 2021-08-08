573a4b24df2419912a0f42cac7f3a18a73a5c643
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-08 19:13:28 +0500

    Add demonstration of atomic_ref

---
+++ Programming/C++/C++20. Get the details/A Quick Overview of C++20/Concurrency/atomics.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+struct StdioManager {
+	void write(const char* message) {
+		std::cout << message << std::endl;
+	}
+};
+
+
+int main() {
+	StdioManager manager;
+	std::atomic_ref<StdioManager> manager(manager);
+
+	std::thread([&manager]() {
+		for(int i = 0; i <= 10; ++i) {
+			manager.write("Hello");
+		}
+	});
+	std::thread([&manager]() {
+		for(int i = 0; i <= 10; ++i) {
+			manager.write("Bye");
+		}
+	});
+}
