452ca26970838e56fdd02cf6b94024775a3dee91
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-09 22:02:21 +0500

    Add example of compound requirements

---
+++ Programming/C++/C++20. Get the details/The Details/Core Language/Concepts/Defining concepts/compound_requirements.cpp
@@ -0,0 +1,31 @@
+#include <concepts>
+#include <iostream>
+
+template <typename T>
+concept Equal = requires(T a, T b) {
+	{ a == b } -> std::convertible_to<bool>;
+	{ a != b } -> std::convertible_to<bool>;
+};
+
+bool areEqual(Equal auto a, Equal auto b) {
+	return a == b;
+}
+
+struct WithoutEqual {
+	bool operator==(const WithoutEqual& other) = delete;
+};
+
+struct WithoutUnequal {
+	bool operator!=(const WithoutUnequal& other) = delete;
+};
+
+int main() {
+	std::cout << areEqual(1, 5) << '\n';
+
+	/*
+
+	bool res = areEqual(WithoutEqual{}, WithoutEqual{});
+	bool res2 = areEqual(WithoutUnequal{}, WithoutUnequal{});
+
+	*/
+}
