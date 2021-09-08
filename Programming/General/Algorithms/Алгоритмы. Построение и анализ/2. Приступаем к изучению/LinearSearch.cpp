02c121e42e1520fbc7b5e855bb0e9a5ba93a2211
PatriotRossii <patriotrossii2019@mail.ru>
2021-09-08 22:50:32 +0500

    Add LinearSearch

---
+++ Programming/General/Algorithms/Алгоритмы. Построение и анализ/2. Приступаем к изучению/LinearSearch.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+
+template<typename T, std::size_t N>
+int LinearSearch(T array[N], T toFind) {
+	for(std::size_t idx = 0; idx != N; ++idx) {
+		if(array[idx] == toFind) return idx;
+	}
+	return -1;
+}
