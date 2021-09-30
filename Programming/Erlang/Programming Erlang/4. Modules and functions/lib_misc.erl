e1bccc9d708db35536005dc3475115650e6dda47
PatriotRossii <patriotrossii2019@mail.ru>
2021-09-30 16:40:18 +0500

    Add solution of second exercise

--- Programming/Erlang/Programming Erlang/4. Modules and functions/lib_misc.erl
+++ Programming/Erlang/Programming Erlang/4. Modules and functions/lib_misc.erl
@@ -1,12 +1,12 @@
 -module(lib_misc).
--export([lib_misc/3]).
+-export([tuple_to_list/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I + 1, Max, F)].
 
 qsort([]) -> [];
 qsort([Pivot|T]) ->
-	qsort([X || X <- T, X < Pivot]) ++ [Pivot] ++ qsort([X || X <- T, X >= Pivot]
+	qsort([X || X <- T, X < Pivot]) ++ [Pivot] ++ qsort([X || X <- T, X >= Pivot]).
 
 pythag(N) ->
 	[
@@ -29,10 +29,13 @@ odds_and_evens(L) ->
 cooler_odds_and_evens(L) ->
 	odds_and_evens_acc(L, [], []).
 
-odds_and_evens_acc([H|T], Odds, Evens] ->
+odds_and_evens_acc([H|T], Odds, Evens) ->
 	case (H rem 2) of
 		1 -> odds_and_evens_acc(T, [H|Odds], Evens);
 		0 -> odds_and_evens_acc(T, Odds, [H|Evens])
 	end;
 odds_and_evens_acc([], Odds, Evens) ->
 	{Odds, Evens}.
+
+tuple_to_list(T) ->
+	[erlang:element(I, T) || I <- lists:seq(1, tuple_size(T))].
