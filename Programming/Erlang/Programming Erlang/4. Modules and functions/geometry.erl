bbac0cebb9bb79751e33c027b96b4d3714b95041
PatriotRossii <patriotrossii2019@mail.ru>
2021-09-30 16:32:55 +0500

    Solve first exercise

--- Programming/Erlang/Programming Erlang/4. Modules and functions/geometry.erl
+++ Programming/Erlang/Programming Erlang/4. Modules and functions/geometry.erl
@@ -1,5 +1,5 @@
 -module(geometry).
--export([area/1]).
+-export([test/0, area/1, perimeter/1]).
 
 test() ->
 	12 = area({rectangle, 3, 4}),
@@ -7,4 +7,23 @@ test() ->
 	tests_worked.
 
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side}) -> Side * Side.
+area({square, Side}) -> Side * Side;
+
+% Exercise 1
+
+area({circle, Radius}) ->
+	math:pi() * Radius * Radius;
+
+area({right_angled_triangle, A, B}) ->
+	A * B.
+
+perimeter({rectangle, A, B}) ->
+	2 * (A + B);
+perimeter({square, A}) ->
+	4 * A;
+perimeter({triangle, A, B, C}) ->
+	A + B + C;
+perimeter({rhombus, A}) ->
+	4 * A;
+perimeter({circle, Radius}) ->
+	2 * math:pi() * Radius.
