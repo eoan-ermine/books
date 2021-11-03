b03e8a887db590c9604e04ef78ea7b3b09049af6
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-03 20:30:08 +0500

    Add solution to exercise

---
+++ Programming/Practice/Stepik/МКН Сириус/Читаем и пишем на языке Kotlin/second.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val letters = arrayOf('L', 'I', 'T', 'T', 'L', 'E', ' ', 'R', 'E', 'D', ' ', 'B', 'O', 'O', 'K')
+    var index = 13
+    while (index < letters.size) {
+        print(letters[index])
+        index++
+    }
+}
