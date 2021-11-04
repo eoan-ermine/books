a3db0f007d5d8a76619cb86d92f4fef93d82d035
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-04 12:32:44 +0500

    Change exactly_one to zero_or_exactly_one

--- Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
+++ Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
@@ -1,4 +1,4 @@
-from pycosat import solve
+from pycosat import solve, itersolve
 from itertools import combinations, product
 import logging
 import sys
@@ -8,6 +8,12 @@ def varnum(row, column):
 	return 8 * row + column + 1
 
 
+def zero_of(literals):
+	clauses = [[l for l in literals]]
+	clauses.extend([-l] for l in literals)
+	return clauses
+
+
 def exactly_one_of(literals):
 	clauses = [[l for l in literals]]
 	for pair in combinations(literals, 2):
@@ -15,10 +21,18 @@ def exactly_one_of(literals):
 	return clauses
 
 
+def one_or_zero_of(literals):
+	clauses = [[l for l in literals] + [-l for l in literals]]
+	for pair in combinations(literals, 2):
+		clauses.append([-l for l in pair])
+	clauses.append([-l for l in literals])
+	return clauses
+
+
 def one_digit_in_every_row(N):
 	clauses = []
 	for row in range(1, N + 1):
-		clauses += exactly_one_of([varnum(row, column)
+		clauses += one_or_zero_of([varnum(row, column)
 								   for column in range(1, N + 1)])
 	return clauses
 
@@ -26,7 +40,7 @@ def one_digit_in_every_row(N):
 def one_digit_in_every_column(N):
 	clauses = []
 	for column in range(1, N + 1):
-		clauses += exactly_one_of([varnum(row, column)
+		clauses += one_or_zero_of([varnum(row, column)
 								   for row in range(1, N + 1)])
 	return clauses
 
@@ -39,7 +53,7 @@ def one_digit_in_every_diagonal(N):
 
 	for row in range(1, N):
 		diagonal = [(row + i, 1 + i) for i in range(0, N) if (row + i <= N) and (1 + i <= N)]
-		clauses += exactly_one_of([varnum(row, column) for row, column in diagonal])
+		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
 		logging.debug(diagonal)
 
 	# diagonal = [(1, 2), (2, 3)] 		diagonal = [(1, 3), (2, 4)]
@@ -47,7 +61,7 @@ def one_digit_in_every_diagonal(N):
 
 	for column in range(2, N):
 		diagonal = [(1 + i, column + i) for i in range(0, N) if (column + i <= N) and (1 + i <= N)]
-		clauses += exactly_one_of([varnum(row, column) for row, column in diagonal])
+		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
 		logging.debug(diagonal)
 	
 	# diagonal = [(8, 1), (7, 2)]		diagonal = [(8, 2), (7, 3)]
@@ -55,7 +69,7 @@ def one_digit_in_every_diagonal(N):
 
 	for column in range(1, N):
 		diagonal = [(N - i, column + i) for i in range(0, N) if (column + i <= N) and (8 - i >= 1)]
-		clauses += exactly_one_of([varnum(row, column) for row, column in diagonal])
+		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
 		logging.debug(diagonal)
 
 	# diagonal = [(2, 1), (1, 2)]		diagonal = [(3, 1), (2, 2), (1, 3)]
@@ -63,7 +77,7 @@ def one_digit_in_every_diagonal(N):
 
 	for row in range(2, N):
 		diagonal = [(row - i, 1 + i) for i in range(0, N) if (row - i >= 1) and (1 + i <= N)]
-		clauses += exactly_one_of([varnum(row, column) for row, column in diagonal])
+		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
 		logging.debug(diagonal)
 
 	return clauses
@@ -76,29 +90,23 @@ def solve_puzzle(n):
 	clauses += one_digit_in_every_column(n)
 	clauses += one_digit_in_every_diagonal(n)
 
-	solution = solve(clauses)
-
-	if isinstance(solution, str):
-		return None
-
-	result = []
-	for row in range(1, n + 1):
-		current_row = []
-		for column in range(1, n + 1):
-			if varnum(row, column) in solution:
-				current_row.append("*")
-			else:
-				current_row.append("X")
-		result.append(current_row)
-	return result
+	for solution in itersolve(clauses):
+		result = []
+		for row in range(1, n + 1):
+			current_row = []
+			for column in range(1, n + 1):
+				if varnum(row, column) in solution:
+					current_row.append("*")
+				else:
+					current_row.append("X")
+			result.append(current_row)
+		yield result
 
 
 if __name__ == "__main__":
 	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 	n = int(input())
 
-	result = solve_puzzle(n)
-	if result:
+	for result in solve_puzzle(n):
 		print("\n".join(["".join([str(e) for e in element]) for element in result]))
-	else:
-		print(-1)
+		print()
