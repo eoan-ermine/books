2643613fb60fb8808c610c1fa7f27b7c0c91fa6b
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-04 11:10:35 +0500

    Shit, my diagonal shit doesn't work

--- Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
+++ Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
@@ -29,14 +29,35 @@ def one_digit_in_every_column():
 								   for row in range(1, 10)])
 	return clauses
 
+
 def one_digit_in_every_diagonal():
 	clauses = []
 	
-	diagonals = []
-	for column in range(1, 9):
-		diagonals.append(
-			[varnum(row + i, row + i) for i in range(8) if row + i <= 8]
-		)
+	# diagonal = [(1, 1), (2, 2) ...]	diagonal = [(2, 1), (3, 2)]
+	# Correct
+
+	for row in range(1, 8):
+		clauses += exactly_one_of([varnum(row + i, 1 + i) for i in range(0, 8) if (row + i <= 8) and (1 + i <= 8)])
+
+	# diagonal = [(1, 2), (2, 3)] 		diagonal = [(1, 3), (2, 4)]
+	# Correct
+
+	for column in range(2, 8):
+		clauses += exactly_one_of([varnum(1 + i, column + i) for i in range(0, 8) if (column + i <= 8) and (1 + i <= 8)])
+	
+	# diagonal = [(8, 1), (7, 2)]		diagonal = [(8, 2), (7, 3)]
+	# Correct
+
+	for column in range(1, 8):
+		clauses += exactly_one_of([varnum(8 - i, column + i) for i in range(0, 8) if (column + i <= 8) and (8 - i >= 1)])
+
+
+	# diagonal = [(2, 1), (1, 2)]		diagonal = [(3, 1), (2, 2), (1, 3)]
+
+	for row in range(2, 8):
+		clauses += exactly_one_of([varnum(row - i, 1 + i) for i in range(0, 8) if (row - i >= 1) and (1 + i <= 8)])
+
+	return clauses
 
 
 def solve_puzzle(puzzle):
@@ -56,6 +77,8 @@ def solve_puzzle(puzzle):
 		current_row = []
 		for column in range(1, 10):
 			if varnum(row, column) in solution:
+				current_row.append("*")
+			else:
 				current_row.append("X")
 		result.append(current_row)
 	return result
