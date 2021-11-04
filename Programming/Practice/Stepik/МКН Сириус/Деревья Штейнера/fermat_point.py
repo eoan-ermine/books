48651d667fcb8134a401cd7a45a28b8d083061cc
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-04 17:42:22 +0500

    Rewrite CoffeeScript program to Python

--- Programming/Practice/Stepik/МКН Сириус/Деревья Штейнера/fermat_point.py
+++ Programming/Practice/Stepik/МКН Сириус/Деревья Штейнера/fermat_point.py
@@ -1,18 +1,4 @@
-def line_intersection(line1, line2):
-    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-       raise Exception('lines do not intersect')
-
-    d = (det(*line1), det(*line2))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
+import math
 
 
 def distance(point1, point2):
@@ -21,18 +7,41 @@ def distance(point1, point2):
 	return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
+def fermatPoint(points):
+	sides = []
+	for i in range(len(points)):
+		sides.append(distance(points[i], points[(i + 1) % len(points)]))
+	squared_sides = [side ** 2 for side in sides]
+
+	p = sum(sides) / 2
+	S = (p * (p - sides[0]) * (p - sides[1]) * (p - sides[2])) ** 0.5
+
+	for i in range(3):
+		a, b, c = sides[i], sides[(i + 1) % 3], sides[(i + 2) % 3]
+		sa, sb, sc = squared_sides[i], squared_sides[(i + 1) % 3], squared_sides[(i + 2) % 3]
+		
+		angle = math.acos((sa + sc - sb) / (2 * a * c))
+		if angle > 2 * math.pi / 3:
+			return points[i]
+
+	bcs = []
+	for i in range(len(points)):
+		a, b, c = sides[i], sides[(i + 1) % 3], sides[(i + 2) % 3]
+		sa, sb, sc = squared_sides[i], squared_sides[(i + 1) % 3], squared_sides[(i + 2) % 3]
+		bcs.append((sa ** 2) - 2 * (b - c) ** 2 + sa*(sb + sc + 4 * (3 ** 0.5) * S))
+
+
+	return (
+	  (bcs[1]*points[0][0] + bcs[2]*points[1][0] + bcs[0]*points[2][0]) / (bcs[0] + bcs[1] + bcs[2]),
+	  (bcs[1]*points[0][1] + bcs[2]*points[1][1] + bcs[0]*points[2][1]) / (bcs[0] + bcs[1] + bcs[2])
+	)
+
+	
 x1, y1, x2, y2, x3, y3 = map(int, input().split())
 
 A = (x1, y1)
 B = (x2, y2)
 C = (x3, y3)
 
-base = distance(B, C)
-
-A_left = (A[0] - base, A[1])
-A_right = (A[0] + base, A[1])
-
-line1 = (A_left, C)
-line2 = (A_right, B)
-
-print(line_intersection(line1, line2))
+T = fermatPoint([A, B, C])
+print(round(T[0], 6), round(T[1], 6))
