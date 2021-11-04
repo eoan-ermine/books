df58fa8ca2e6784e46b75154c3b7bbe0f5484a5d
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-04 19:47:47 +0500

    Upload solution of SHIT exercise

--- Programming/Practice/Stepik/МКН Сириус/Деревья Штейнера/triangle.py
+++ Programming/Practice/Stepik/МКН Сириус/Деревья Штейнера/triangle.py
@@ -3,7 +3,7 @@ import sys
 
 
 def output(point):
-	print("{:.6} {:.6}".format(point[0] if point[0] != -0 else -point[0], C[1]))
+	print("{} {}".format(point[0] if point[0] != -0 else -point[0], C[1]))
 
 
 def distance(point1, point2):
@@ -12,17 +12,43 @@ def distance(point1, point2):
 	return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-def relation(A, B, C):
-	x1, y1 = A
-	x2, y2 = B
-	x3, y3 = C
+def is_opposite_sides(a, b, f, t):
+	ax, ay = a
+	bx, by = b
 
-	rel = (x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)
-	if rel < 0:
-		return -1
-	if rel > 0:
-		return 1
-	return 0
+	x1, y1 = f
+	x2, y2 = t
+
+	return ((y1 - y2) * (ax - x1) + (x2 - x1) * (ay - y1)) * ((y1 - y2) * (bx - x1) + (x2 - x1) * (by - y1)) < 0
+
+
+def get_point(A, B, right):
+	dx = B[0] - A[0]
+	dy = B[1] - A[1]
+	length = (dx * dx + dy*dy) ** 0.5
+
+	dirX = dx / length
+	dirY = dy / length
+
+	height = (3 ** 0.5) / 2 * length
+
+	cx = A[0] + dx * 0.5
+	cy = A[1] + dy * 0.5
+
+	pDirX = -dirY
+	pDirY = dirX
+
+	rx = 0
+	ry = 0
+
+	if right:
+		rx = cx + height * pDirX
+		ry = cy + height * pDirY
+	else:
+		rx = cx - height * pDirX
+		ry = cy - height * pDirY
+
+	return (round(rx, 6), round(ry, 6))
 
 
 x1, y1, x2, y2 = map(int, input().split())
@@ -33,10 +59,12 @@ B = (x2, y2)
 side = distance(A, B)
 angle = 1.0471975511965977461542144610931676280657231331250352736583148641
 
-zero_relation = relation(A, B, (0,0))
+zero = (0, 0)
 
-C = (
-	round(A[0] + math.cos(angle) * side, 6),
-	round(A[1] + math.sin(angle) * side, 6)
-)
-output(C)
+C = get_point(A, B, False)
+if is_opposite_sides(zero, C, A, B):
+	output(C)
+else:
+	C = get_point(A, B, True)
+	if is_opposite_sides(zero, C, A, B):
+		output(C)
