a6c57fa007642532741e10442852d616fb424ff3
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-04 13:10:09 +0500

    Modify to correct output format

--- Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
+++ Programming/Practice/Stepik/МКН Сириус/SAT-солверы/queens.py
@@ -1,6 +1,5 @@
-from pycosat import solve, itersolve
-from itertools import combinations, product
-import logging
+from pycosat import solve
+from itertools import combinations
 import sys
 
 
@@ -52,33 +51,25 @@ def one_digit_in_every_diagonal(N):
 	# Correct
 
 	for row in range(1, N):
-		diagonal = [(row + i, 1 + i) for i in range(0, N) if (row + i <= N) and (1 + i <= N)]
-		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
-		logging.debug(diagonal)
+		clauses += one_or_zero_of([varnum(row + i, 1 + i) for i in range(0, N) if (row + i <= N) and (1 + i <= N)])
 
 	# diagonal = [(1, 2), (2, 3)] 		diagonal = [(1, 3), (2, 4)]
 	# Correct
 
 	for column in range(2, N):
-		diagonal = [(1 + i, column + i) for i in range(0, N) if (column + i <= N) and (1 + i <= N)]
-		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
-		logging.debug(diagonal)
+		clauses += one_or_zero_of([varnum(1 + i, column + i) for i in range(0, N) if (column + i <= N) and (1 + i <= N)])
 	
 	# diagonal = [(8, 1), (7, 2)]		diagonal = [(8, 2), (7, 3)]
 	# Correct
 
 	for column in range(1, N):
-		diagonal = [(N - i, column + i) for i in range(0, N) if (column + i <= N) and (8 - i >= 1)]
-		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
-		logging.debug(diagonal)
+		clauses += one_or_zero_of([varnum(N - i, column + i) for i in range(0, N) if (column + i <= N) and (8 - i >= 1)])
 
 	# diagonal = [(2, 1), (1, 2)]		diagonal = [(3, 1), (2, 2), (1, 3)]
 	# Correct
 
 	for row in range(2, N):
-		diagonal = [(row - i, 1 + i) for i in range(0, N) if (row - i >= 1) and (1 + i <= N)]
-		clauses += one_or_zero_of([varnum(row, column) for row, column in diagonal])
-		logging.debug(diagonal)
+		clauses += one_or_zero_of([varnum(row - i, 1 + i) for i in range(0, N) if (row - i >= 1) and (1 + i <= N)])
 
 	return clauses
 
@@ -96,22 +87,16 @@ def solve_puzzle(n):
 
 	result = []
 	for row in range(1, n + 1):
-		current_row = []
 		for column in range(1, n + 1):
 			if varnum(row, column) in solution:
-				current_row.append("*")
-			else:
-				current_row.append("X")
-		result.append(current_row)
-
+				result.append(column - 1)
 	return result
 
 if __name__ == "__main__":
-	logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 	n = int(input())
 
 	result = solve_puzzle(n)
 	if result:
-		print("\n".join(["".join([str(e) for e in element]) for element in result]))
+		print(" ".join([str(e) for e in result]))
 	else:
 		print(-1)
