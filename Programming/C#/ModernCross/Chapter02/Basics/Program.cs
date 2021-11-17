11b79b33335a79998ea97ede08e407940a273dc9
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-17 20:13:04 +0500

    Add Basics program

---
+++ Programming/C#/ModernCross/Chapter02/Basics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+
+class Program {
+    static void Main(string[] args) {
+        System.Data.DataSet ds;
+        System.Net.Http.HttpClient client;
+        
+        foreach(var r in Assembly.GetEntryAssembly()
+            .GetReferencedAssemblies())
+        {
+            var a = Assembly.Load(new AssemblyName(r.FullName));
+            
+            int methodCount = 0;
+            foreach(var t in a.DefinedTypes) {
+                methodCount += t.GetMethods().Count();
+            }
+
+            Console.WriteLine(
+                "{0:N0} types with {1:N0} methods in {2} assembly.",
+                arg0: a.DefinedTypes.Count(),
+                arg1: methodCount,
+                arg2: r.Name
+            );
+        }
+    }
+}
